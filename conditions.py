d04244190e3a7baa93bb862a3f3b3a398b1077e6
Raviraj Wadnerkar <ravirajwadnerkar@gmail.com>
2020-08-03 00:45:33 +0530

    conditions and loops .py
    done my #day2 contribution

---
+++ conditions.py
@@ -0,0 +1,15 @@
+name='Bob'
+if name =='alice':
+    print('hi alice')
+print('Done')
+
+
+print('Enter a name')
+name = input()
+if name != '':
+    print('Thank you for entering a name.')
+else:
+    print('You did not enter a name.')
+
+
+
